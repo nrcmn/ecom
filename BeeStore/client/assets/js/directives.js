3ac0a0d5836137227cb3d038f9208672deeae326
Carl-fridrix-zendolf-IV <social.neet@gmail.com>
2015-11-26 11:26:01 +0300

    Keyboard upd

--- BeeStore/client/assets/js/directives.js
+++ BeeStore/client/assets/js/directives.js
@@ -110,6 +110,18 @@ angular.module('directives', [])
         }
     })
 
+    // basket form
+    .directive('field', function () {
+        return {
+            controller: function ($scope, $element, $attrs) {
+                $element.on('focus', function () { // listen focus on input
+                    window.selectedInput = $attrs.field; // set global variable with input model name
+                })
+            }
+        }
+    })
+
+    /* --- KEYBOARD BLOCK --- */
     .directive('keyboard', function () {
         return {
             templateUrl: 'templates/keyboard.html',
@@ -123,16 +135,7 @@ angular.module('directives', [])
         }
     })
 
-    .directive('field', function () {
-        return {
-            controller: function ($scope, $element, $attrs) {
-                $element.on('focus', function () {
-                    window.selectedInput = $attrs.field;
-                })
-            }
-        }
-    })
-
+    // keyboard buttons
     .directive('letter', function () {
         return {
             restrict: 'C',
@@ -143,6 +146,119 @@ angular.module('directives', [])
                     }
 
                     $scope.form[window.selectedInput] += $element[0].innerText;
+                    $scope.$apply();
+                })
+            }
+        }
+    })
+    .directive('symbol', function () {
+        return {
+            restrict: 'C',
+            controller: function ($scope, $element, $attrs) {
+                $element.on('click', function () {
+                    if ($scope.form[window.selectedInput] == undefined) {
+                        $scope.form[window.selectedInput] = ''
+                    }
+
+                    $scope.form[window.selectedInput] += $element[0].innerText;
+                    $scope.$apply();
+                })
+            }
+        }
+    })
+    .directive('delete', function () {
+        return {
+            restrict: 'C',
+            controller: function ($scope, $element, $attrs) {
+                $element.on('click', function () {
+                    if ($scope.form[window.selectedInput] == undefined) {
+                        $scope.form[window.selectedInput] = ''
+                    }
+
+                    var a = $scope.form[window.selectedInput].split('');
+                    a.pop();
+
+                    $scope.form[window.selectedInput] = a.join('');
+                    $scope.$apply();
+                })
+            }
+        }
+    })
+    .directive('capslock', function () {
+        return {
+            restrict: 'C',
+            controller: function ($scope, $element, $attrs) {
+                $element.on('click', function () {
+                    var letters = document.querySelectorAll('.letter');
+                    var uppercase = document.querySelectorAll('.uppercase');
+
+                    if (uppercase.length != 0) {
+                        for (var i = 0; i < uppercase.length; i++) {
+                            var arr = uppercase[i].className.split(' ');
+                            arr.splice(arr.indexOf('uppercase'), 1);
+
+                            uppercase[i].className = arr.join('');
+                        }
+
+                        return true
+                    }
+
+                    for (var i = 0; i < letters.length; i++) {
+                        letters[i].className += ' uppercase';
+                    }
+                })
+            }
+        }
+    })
+    .directive('shift', function () {
+        return {
+            restrict: 'C',
+            controller: function ($scope, $element, $attrs) {
+                $element.on('click', function () {
+                    var letters = document.querySelectorAll('.letter');
+                    var uppercase = document.querySelectorAll('.uppercase');
+
+                    if (uppercase.length != 0) {
+                        for (var i = 0; i < uppercase.length; i++) {
+                            var arr = uppercase[i].className.split(' ');
+                            arr.splice(arr.indexOf('uppercase'), 1);
+
+                            uppercase[i].className = arr.join('');
+                        }
+
+                        return true
+                    }
+
+                    for (var i = 0; i < letters.length; i++) {
+                        letters[i].className += ' uppercase';
+                    }
+
+                    var listen = $scope.$watch('form', function () {
+                        var uppercase = document.querySelectorAll('.uppercase');
+                        for (var i = 0; i < uppercase.length; i++) {
+                            var arr = uppercase[i].className.split(' ');
+                            arr.splice(arr.indexOf('uppercase'), 1);
+
+                            uppercase[i].className = arr.join('');
+                        }
+
+                        listen();
+                    })
+                })
+            }
+        }
+    })
+
+    .directive('space', function () {
+        return {
+            restrict: 'C',
+            controller: function ($scope, $element, $attrs) {
+                $element.on('click', function () {
+                    if ($scope.form[window.selectedInput] == undefined) {
+                        $scope.form[window.selectedInput] = ''
+                    }
+
+                    $scope.form[window.selectedInput] += ' ';
                     $scope.$apply();
                 })
             }
