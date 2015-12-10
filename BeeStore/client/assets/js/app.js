6c6c8296676fd574ce650db023606d178108015c
Carl-fridrix-zendolf-IV <social.neet@gmail.com>
2015-12-10 12:36:22 +0300

    bug fix

--- BeeStore/client/assets/js/app.js
+++ BeeStore/client/assets/js/app.js
@@ -152,8 +152,8 @@ angular.module('BeeStore', ['ui.router','ngAnimate', 'foundation', 'foundation.d
     .run(function ($rootScope, FoundationApi, $state) {
         FastClick.attach(document.body);
 
-        unacceptableCategories = [6, 5, 4, 101, 15, 202, 23, 24, 164, 78, 80, 79, 166, 162, 165, 71, 70, 77, 122, 121, 182, 93, 86, 85, 90, 87, 163, 10]; // unacceptable categories ids
-        
+        unacceptableCategories = [6, 5, 4, 101, 15, 202, 23, 24, 164, 78, 80, 79, 166, 162, 165, 71, 70, 77, 122, 121, 182, 93, 86, 85, 90, 87, 163]; // unacceptable categories ids
+
         window.api_key = '852bff3ff459f9886729b9de223e8a0340ce008b',
             market_region = 98082,
             filter = {},
